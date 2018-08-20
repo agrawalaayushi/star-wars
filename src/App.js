e73e81423034dbd72ae73d590a388f0119b1d15c
agrawal.aayushi <agrawal.aayushi13@.com>
2018-08-21 05:01:55 +0530

    Redux-store setup done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import logo from './logo.svg';
+import { simpleAction } from './actions/action';
+
 import './App.css';
 
 class App extends Component {
+
+  simpleAction(event){
+    this.props.simpleAction();
+   }
+
   render() {
     return (
       <div className="App">
@@ -13,9 +22,23 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button onClick={(e) => this.simpleAction(e)}>Test redux action</button>
+        <pre>
+          {
+            JSON.stringify(this.props)
+          }
+        </pre>
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  ...state
+ })
+
+ const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+ })
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
+import configureStore from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const target = document.getElementById('root');
+
+ReactDOM.render(
+  <Provider store={configureStore()}>
+   <App />
+  </Provider>,
+  target
+ );
+ registerServiceWorker();
